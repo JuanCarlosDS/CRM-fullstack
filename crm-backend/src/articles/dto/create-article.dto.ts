05c11519b27dbd11475b74c283035be1d533badb
Juanca <juank060790@gmail.com>
2024-06-25 20:59:52 -0600

    Update DTO article and tasks

--- crm-backend/src/articles/dto/create-article.dto.ts
+++ crm-backend/src/articles/dto/create-article.dto.ts
@@ -1,13 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsBoolean, IsNotEmpty } from 'class-validator';
 import { User } from 'src/users/entities/user.entity';
 
 export class CreateArticleDto {
+  @ApiProperty({ example: 'Article Title' })
   @IsNotEmpty()
   title: string;
 
+  @ApiProperty({ example: 'Article content ' })
   @IsNotEmpty()
   content: string;
 
+
+  @ApiProperty({ example: true })
   @IsBoolean()
   published: boolean;
 
--- crm-backend/src/tasks/dto/create-task.dto.ts
+++ crm-backend/src/tasks/dto/create-task.dto.ts
@@ -1,21 +1,28 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsDateString, IsNotEmpty, IsOptional } from 'class-validator';
 
 export class CreateTaskDto {
+    @ApiProperty({ example: 'Task Title' })
     @IsNotEmpty()
     title: string;
 
+    @ApiProperty({ example: 1 })
     @IsNotEmpty()
     author: number;
 
+    @ApiProperty({ example: 2 })
     @IsNotEmpty()
     assignee: number;
 
+    @ApiProperty({ example: 'Task Description', required: false })
     @IsOptional()
     description?: string;
 
+    @ApiProperty({ example: 'Task Status', required: false })
     @IsOptional()
     status?: string;
 
+    @ApiProperty({ example: '2022-12-31', required: false })
     @IsOptional()
     @IsDateString()
     dueDate?: Date;
