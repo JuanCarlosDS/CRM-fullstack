b23781bb69b29710e422e2b980b5aa11bd87b7e2
Juanca <juank060790@gmail.com>
2024-06-27 19:39:44 -0600

    Products update

--- crm-backend/src/products/products.service.ts
+++ crm-backend/src/products/products.service.ts
@@ -3,8 +3,10 @@ import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
-import { Repository } from 'typeorm';
+import { DeepPartial, Repository } from 'typeorm';
 import { User } from 'src/users/entities/user.entity';
+import { EntityCondition } from 'src/utils/types/entity-condition.type';
+import { NullableType } from 'src/utils/types/nullable.type';
 
 @Injectable()
 export class ProductsService {
@@ -15,7 +17,6 @@ export class ProductsService {
 
   create(createProductDto: CreateProductDto, user: User): Promise<Product> {
     createProductDto.user = user;
-    console.log('createProductDto :>> ', createProductDto);
     const newProduct = this.productsRepository.save(
       this.productsRepository.create(createProductDto),
     );
@@ -27,15 +28,31 @@ export class ProductsService {
     return this.productsRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  findOne(fields: EntityCondition<Product>): Promise<NullableType<Product>> {
+    return this.productsRepository.findOne({
+      where: fields,
+    });
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  update(id: Product['id'], payload: DeepPartial<Product>): Promise<Product> {
+    console.log('payload :>> ', payload);
+    return this.productsRepository.save(
+      this.productsRepository.create({
+        id,
+        ...payload,
+      }),
+    );
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: number) {
+    try {
+      const result = await this.productsRepository.delete(id);
+      if (result.affected === 0) {
+        return { message: `Product with ID ${id} not found` };
+      }
+      return { message: `Product with ID ${id} successfully deleted` };
+    } catch (error) {
+      throw new Error(`Error deleting product with ID ${id}: ${error.message}`);
+    }
   }
 }
